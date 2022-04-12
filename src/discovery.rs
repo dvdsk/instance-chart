538fb1f7bf7055bab4747f912338208c63861bfa
dvdsk <noreply@davidsk.dev>
2022-04-12 18:31:09 +0200

    allow JoinError::is_cancelled if maintain returns

--- src/discovery.rs
+++ src/discovery.rs
@@ -6,12 +6,26 @@ use tracing::info;
 use crate::Chart;
 use crate::chart::{handle_incoming, broadcast_periodically};
 
+trait AcceptErr<T, E> {
+    fn accept_err_with(self, f: impl FnOnce(&E) -> bool) -> Result<Option<T>, E>;
+}
+
+impl<T, E> AcceptErr<T, E> for Result<T, E> {
+    fn accept_err_with(self, f: impl FnOnce(&E) -> bool) -> Result<Option<T>, E> {
+        match self {
+            Ok(v) => Ok(Some(v)),
+            Err(e) if f(&e) => Ok(None),
+            Err(e) => Err(e),
+        }
+    }
+}
+
 #[tracing::instrument]
 pub async fn maintain(chart: Chart) {
     let f1 = tokio::spawn(handle_incoming(chart.clone()));
     let f2 = tokio::spawn(broadcast_periodically(chart, Duration::from_secs(10)));
-    f1.await.unwrap();
-    f2.await.unwrap();
+    f1.await.accept_err_with(|e| e.is_cancelled()).unwrap();
+    f2.await.accept_err_with(|e| e.is_cancelled()).unwrap();
     unreachable!("maintain never returns")
 }
 
